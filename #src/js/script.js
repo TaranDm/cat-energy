628cd0b7457a09291403c7bc39a849f4731779e8
Dmitriy <dmitriy.taran1@gmail.com>
2020-09-16 17:56:05 +0300

    add block axample

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,25 +1,35 @@
-let burger_link = document.querySelector(".icon-menu");
-let nav_text = document.querySelector(".menu__body");
-let scroll = document.querySelector ("body");
-burger_link.addEventListener("click", function (e) {
-    e.preventDefault();
-    burger_link.classList.toggle("_active");
-    nav_text.classList.toggle("_active");
 
-    if (nav_text.classList.contains('_active')) {
-        scroll.style.overflow = "hidden";
-    }
-    else {
-        scroll.style.overflow = "visible";
-    }
+$(document).ready(function () {
+    let burger_link = document.querySelector(".icon-menu");
+    let nav_text = document.querySelector(".menu__body");
+    let scroll = document.querySelector ("body");
+    burger_link.addEventListener("click", function (e) {
+        e.preventDefault();
+        burger_link.classList.toggle("_active");
+        nav_text.classList.toggle("_active");
 
+        if (nav_text.classList.contains('_active')) {
+            scroll.style.overflow = "hidden";
+        }
+        else {
+            scroll.style.overflow = "visible";
+        }
+
+    });
+    window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+            evt.preventDefault();
+            burger_link.classList.remove("_active");
+            nav_text.classList.remove("_active");
+            scroll.style.overflow = "visible"
+        }
+
+    });
+
+    $( function() {
+        $( "#slider" ).slider();
+    } );
 });
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-        evt.preventDefault();
-        burger_link.classList.remove("_active");
-        nav_text.classList.remove("_active");
-        scroll.style.overflow = "visible"
-    }
+/*slider range*/
 
-});
+
